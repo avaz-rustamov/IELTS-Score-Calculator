6b0dafbc53cc84b8d5dee8c9ee966f8b7840fe49
avaz-rustamov <avaz.dev@icloud.com>
2022-07-26 02:20:24 +0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let num = Number(prompt("Listening darajangizni kiriting..."));
+let num2 = Number(prompt("Reading darajangizni kiriting..."));
+let num3 = Number(prompt("Writing darajangizni kiriting..."));
+let num4 = Number(prompt("Speaking darajangizni kiriting..."));
+const total = num + num2 + num3 + num4;
+console.log(total);
+let result = alert(`Sizning IELTS darajangiz ${total / 4}`);
